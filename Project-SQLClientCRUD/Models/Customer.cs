2a338c4991e80e228078972d985744d5b71669aa
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-02-22 14:50:41 +0100

    Feat: AddNewCustomer and OBS(Fixed the allt the variables in all the methods)

--- Project-SQLClientCRUD/Models/Customer.cs
+++ Project-SQLClientCRUD/Models/Customer.cs
@@ -15,6 +15,7 @@ namespace Project_SQLClientCRUD.Models
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        public string Country { get; set; }
         public string PostalCode { get; set; }
         public string Phone { get; set; }
         public string Fax { get; set; }
