f5185684d52baa2aa5af8954576549687f651c35
Tommy Pham <60743602+tommy-ph@users.noreply.github.com>
2023-02-23 15:20:46 +0100

    Feat: Mehtod that shows Customers who are the highest spenders (total in invoice table is the largest).

---
+++ Project-SQLClientCRUD/Models/CustomerSpender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_SQLClientCRUD.Models
+{
+    public class CustomerSpender
+    {
+        public int CustomerId { get; set; }
+        public Decimal TotalTopSpent { get; set; }
+    }
+}
